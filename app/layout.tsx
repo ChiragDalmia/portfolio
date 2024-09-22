5700baf3745d50864d82bf4e9e47473a61f423fc
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-09-22 13:11:07 -0400

    complete styles for the blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en" className={`${inter.className} [scrollbar-gutter:stable]`}>
       <body className="antialiased tracking-tight">
         <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8">
-          <main className="max-w-[60ch] mx-auto w-full space-y-6">
+          <main className="pt-12 max-w-[60ch] mx-auto w-full space-y-6">
             {children}
           </main>
           <Footer />
