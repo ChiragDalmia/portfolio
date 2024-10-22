403ddb8b84fd854df21b7ab2729ba1d4e862657d
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-22 14:04:43 -0400

    addded not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page Under Construction</h2>
+      <p>I am working on something great. Please check back soon!</p>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+}
