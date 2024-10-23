0c3b3d43f2b13bcee462029ba686fb77588bc566
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-22 21:27:13 -0400

    move projects in a diff file

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,8 @@
+import { projectsData } from "@/lib/projects";
 import Link from "next/link";
 import React from "react";
 
-const projectsData = [
-  {
-    name: "✨Photosynth-AI",
-    url: "https://photosynthai.cloud/",
-    description: "AI-powered SaaS for quick image transformations",
-  },
-  {
-    name: "ScanTerra",
-    url: "https://scanterra.vercel.app/",
-    description:
-      "Scan barcode to evaluate product's carbon footprint using AI.",
-  },
-  {
-    name: "Quicture",
-    url: "https://sharethephotoswithus.us/",
-    description:
-      "Peer-to-peer image sharing platform that preserves quality, with temporary cloud storage options.",
-  },
-  {
-    name: "✨HackCanada",
-    url: "https://hackcanada.org/",
-    description:
-      "The most, 'are-you-sure-you-want-this?' design request I’ve ever tackled.",
-  },
-  {
-    name: "Old Portfolio",
-    url: "https://chiragdalmia.me/",
-    description: "no one cares but here is my old portfolio",
-  },
-  {
-    name: "VanillaJs Corner",
-    url: "https://myvanillajs.netlify.app/",
-    description:
-      "A collection of simple JavaScript projects I made as a beginner",
-  },
-];
+
 
 const Page = () => {
   return (
---
+++ lib/projects.ts
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    name: "✨Photosynth-AI",
+    url: "https://photosynthai.cloud/",
+    description: "AI-powered SaaS for quick image transformations",
+  },
+  {
+    name: "ScanTerra",
+    url: "https://scanterra.vercel.app/",
+    description:
+      "Scan barcode to evaluate product's carbon footprint using AI.",
+  },
+  {
+    name: "ARchatPet",
+    url: "https://archatpet.vercel.app/landing",
+    description:
+      "Turn a simple photo of your pet into a 3D model and chat with it in an AR world.",
+  },
+  {
+    name: "Quicture",
+    url: "https://sharethephotoswithus.us/",
+    description:
+      "Peer-to-peer image sharing platform that preserves quality, with temporary cloud storage options.",
+  },
+  {
+    name: "✨HackCanada",
+    url: "https://hackcanada.org/",
+    description:
+      "The most, 'are-you-sure-you-want-this?' design request I’ve ever tackled.",
+  },
+  {
+    name: "Old Portfolio",
+    url: "https://chiragdalmia.me/",
+    description: "no one cares but here is my old portfolio",
+  },
+  {
+    name: "VanillaJs Corner",
+    url: "https://myvanillajs.netlify.app/",
+    description:
+      "A collection of simple JavaScript projects I made as a beginner",
+  },
+];
