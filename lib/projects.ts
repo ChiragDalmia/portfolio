e68ab63909e6c42de8edf6f21896f3da861e07be
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-23 00:57:39 -0400

    Add projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,14 +1,9 @@
 export const projectsData = [
   {
-    name: "✨Photosynth-AI",
-    url: "https://photosynthai.cloud/",
-    description: "AI-powered SaaS for quick image transformations",
-  },
-  {
-    name: "ScanTerra",
-    url: "https://scanterra.vercel.app/",
+    name: "✨HackCanada",
+    url: "https://staging.hackcanada.org/",
     description:
-      "Scan barcode to evaluate product's carbon footprint using AI.",
+      "The most, 'are-you-sure-you-want-this?' design request I’ve ever tackled.",
   },
   {
     name: "ARchatPet",
@@ -17,21 +12,26 @@ export const projectsData = [
       "Turn a simple photo of your pet into a 3D model and chat with it in an AR world.",
   },
   {
+    name: "ScanTerra",
+    url: "https://scanterra.vercel.app/",
+    description:
+      "Scan barcode to evaluate product's carbon footprint using AI.",
+  },
+  {
     name: "Quicture",
     url: "https://sharethephotoswithus.us/",
     description:
       "Peer-to-peer image sharing platform that preserves quality, with temporary cloud storage options.",
   },
   {
-    name: "✨HackCanada",
-    url: "https://hackcanada.org/",
-    description:
-      "The most, 'are-you-sure-you-want-this?' design request I’ve ever tackled.",
+    name: "Old Portfolio",
+    url: "https://chiragdalmia.vercel.app/",
+    description: "no one cares but here is my old portfolio",
   },
   {
-    name: "Old Portfolio",
-    url: "https://chiragdalmia.me/",
-    description: "no one cares but here is my old portfolio",
+    name: "✨Photosynth-AI",
+    url: "https://photosynthai.cloud/",
+    description: "AI-powered SaaS for quick image transformations",
   },
   {
     name: "VanillaJs Corner",
