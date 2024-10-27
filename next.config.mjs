9e824bf00c1d77896009394f3d1f5b5f3ea07d90
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-10-27 17:00:46 -0400

    remove decaprecated values

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,14 @@ const nextConfig = {
     mdxRs: true,
   },
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
