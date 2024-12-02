167bc656e19b7ed6f40132f1a92e2ccfcf417ff7
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-12-02 10:00:00 -0500

    fix deploment error

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,24 @@ import React from "react";
 const Page = () => {
   return (
     <>
-      <h1>Hey, I'm Chirag.</h1>
+      <h1>Hey, I&apos;m Chirag.</h1>
       <section aria-labelledby="about-me">
         <h2 id="about-me" className="sr-only">
           About Me
         </h2>
         <ul>
           <li>
-            Front-end wizard 🧙‍♂️ crafting{" "}
+            I am a Front-end dev crafting{" "}
             <Link href="/projects">cool web stuff</Link>. I love{" "}
             <Link href="/yap-corner">chatting about code</Link> and playing with
-            3D magic ✨. Drop by my{" "}
+            3D Design. Drop by my{" "}
             <Link href="/guestlog">digital guestbook</Link> and say hi!
-            Currently, I dream about bringing AR to the web for everyone. No
-            fancy glasses required! 
           </li>
         </ul>
       </section>
 
       <p>
-        Got a fun project? Let's team up! Find me on{" "}
+        Got a fun project? Let&apos;s team up! Find me on{" "}
         <Link href="https://x.com/dotchirag">Twitter</Link> for tech talk and
         memes.
       </p>
