55b2c5e43b3f6e5dd3da6b56f49c2aec8e97f2a6
ChiragDalmia <chiragdalmia11@gmail.com>
2026-07-03 18:22:24 -0400

    remove unused files

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-
-
-
- // Function to get the first name and format it
- export const formatFirstName = (fullName: string) => {
-    const firstName = fullName.split(" ")[0];
-    return firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-  };
